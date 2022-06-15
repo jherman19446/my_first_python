ff87457913d2a4181d13c21db47ee7f5131f1d9f
Jon Herman <jherman@localhost.localdomain>
2022-06-15 14:36:48 -0400

    formatted

--- how_old_am_I.py
+++ how_old_am_I.py
@@ -2,17 +2,18 @@ from datetime import date
 print("1. My birthday is today ")
 print("2. My birth year is ")
 options = input("Please choose option 1 or 2 ")
+
+
 def age():
     current_year = int(date.today().year)
     birth_year = int(input("What year were you born? "))
     years = current_year - birth_year
     print("You are " + str(years) + " years old")
 
+
 if options == "1":
-    
+
     print("Happy birthday!!!")
     age()
 elif options == "2":
-   age()
-
-
+    age()
